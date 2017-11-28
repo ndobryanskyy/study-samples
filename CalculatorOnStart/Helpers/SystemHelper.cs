33085e25dd9f0ed01fbd371595b98b283b96b169
Nikita Dobryanskyy <ndobryanskyy@lohika.com>
2017-11-29 00:16:36 +0200

    -- Implemented adding to startup

---
+++ CalculatorOnStart/Helpers/SystemHelper.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Win32;
+
+namespace CalculatorOnStart.Helpers
+{
+    public class SystemHelper
+    {
+        private readonly string _appName;
+        private readonly string _executablePath;
+
+        private readonly RegistryKey _registryKey;
+
+
+        public SystemHelper(string appName, string executablePath)
+        {
+            _appName = appName;
+            _executablePath = executablePath;
+
+            _registryKey =
+                Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", true);
+        }
+
+        public bool IsStartupItem
+        {
+            get => _registryKey.GetValue(_appName) != null;
+            set
+            {
+                if (value) _registryKey.SetValue(_appName, _executablePath);
+                else _registryKey.DeleteValue(_appName, false);
+            }
+        }
+    }
+}
--- CalculatorOnStart/MainWindow.xaml.cs
+++ CalculatorOnStart/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using CalculatorOnStart.Helpers;
+using Path = System.IO.Path;
 
 namespace CalculatorOnStart
 {
@@ -20,9 +22,21 @@ namespace CalculatorOnStart
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly SystemHelper _systemHelper;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            var currentDomain = AppDomain.CurrentDomain;
+            var executablePath = Path.Combine(currentDomain.BaseDirectory, currentDomain.FriendlyName);
+
+            _systemHelper = new SystemHelper(Title, executablePath);
+        }
+
+        private void SetStartupOnLogin(object sender, RoutedEventArgs e)
+        {
+            _systemHelper.IsStartupItem = !_systemHelper.IsStartupItem;
         }
     }
 }
