b595786a3e84aff8121b618f5100186b5e3ab881
Nikita Dobryanskyy <ndobryanskyy@lohika.com>
2017-11-29 00:17:21 +0200

    -- Implmented directory walking

---
+++ CustomDir/CustomDir.py
@@ -0,0 +1,11 @@
+import os
+
+print('+', os.getcwd(), sep='')
+
+for root, dirs, files in os.walk('.'):
+    for name in files:
+        depth = root.count(os.path.sep) + 2
+        print(depth*' ', name, sep='')
+    for name in dirs:
+        depth = root.count(os.path.sep) + 1
+        print(depth*' ', '+', name, sep='')
