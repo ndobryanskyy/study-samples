c282d524c69e5043f130c2ba6803205bbd6409ce
ndobryanskyy <ndobryanskyy@gmail.com>
2017-11-29 09:21:43 +0200

    -- Basic screen capture implementation

---
+++ ScreenCapture/MainWindowViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using Prism.Commands;
+using Prism.Mvvm;
+
+namespace ScreenCapture
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        public MainWindowViewModel()
+        {
+            SaveSnapshot = new DelegateCommand(OnSaveSnapshot);
+        }
+
+        private async void OnSaveSnapshot()
+        {
+            var service = new ScreenCaptureService();
+
+            var window = Application.Current.MainWindow;
+            window.WindowState = WindowState.Minimized;
+            await Task.Delay(TimeSpan.FromSeconds(1));
+
+            var bitmap = service.TakeScreenSnapshot();
+            try
+            {
+                bitmap.Save("D:\\Screenshot.jpg");
+                window.WindowState = WindowState.Normal;
+            }
+            catch
+            {
+                
+            }
+        }
+
+        public ICommand SaveSnapshot { get; }
+    }
+}
---
+++ ScreenCapture/ScreenCaptureService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Windows;
+
+namespace ScreenCapture
+{
+    public class ScreenCaptureService
+    {
+        public Bitmap TakeScreenSnapshot()
+        {
+            var screenWidth = (int) SystemParameters.PrimaryScreenWidth;
+            var screenHeight = (int)SystemParameters.PrimaryScreenHeight;
+
+            var bitmap = new Bitmap(
+                screenWidth, 
+                screenHeight,
+                PixelFormat.Format32bppArgb);
+
+            using (var g = Graphics.FromImage(bitmap))
+            {
+                g.CopyFromScreen(0, 0, 0, 0, bitmap.Size);
+
+                g.SmoothingMode = SmoothingMode.AntiAlias;
+                g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                g.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                float textWidth = 200;
+                float textHeight = 50;
+
+                g.DrawString(DateTime.Today.ToString("D"),
+                    new Font("Tahoma", 16), Brushes.DarkRed, new RectangleF(screenWidth - textWidth - 50, screenHeight - textHeight - 50, textWidth, textHeight));
+
+                g.Flush();
+            }
+
+            return bitmap;
+        }
+
+        [System.Runtime.InteropServices.DllImport("gdi32.dll")]
+        private static extern bool DeleteObject(IntPtr onj);
+    }
+}
