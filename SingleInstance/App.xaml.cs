c812dbb58db990ca19fa789ec60c31edb114f0fb
ndobryanskyy <ndobryanskyy@gmail.com>
2017-11-29 03:30:30 +0200

    -- Implemented single instance application

---
+++ SingleInstance/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace SingleInstance
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var guard = new MutexGuard();
+            if (!guard.IsSingleInstance())
+            {
+                MessageBox.Show("Only one for you Tiger...", "Not so fast");
+                Shutdown(1);
+            }
+
+            base.OnStartup(e);
+        }
+    }
+}
---
+++ SingleInstance/MutexGuard.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
+
+namespace SingleInstance
+{
+    public class MutexGuard
+    {
+        private const string MutexName = "SINGLEINSTANCE";
+
+        private Mutex _mutex;
+
+        public bool IsSingleInstance()
+        {
+            if (!Mutex.TryOpenExisting(MutexName, out var _))
+            {
+                _mutex = new Mutex(true, MutexName);
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
