2d873346444a31178832aff1966cc1ef071bf753
Nikita Dobryanskyy <ndobryanskyy@lohika.com>
2017-11-29 02:39:58 +0200

    -- Placeholder implementation of the client.

---
+++ Requester/Client/ApiClient.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Flurl.Http;
+
+namespace Requester.Client
+{
+    public class ApiClient
+    {
+        public async Task<HttpResponseMessage> PostString(string endpoint, string data)
+        {
+            return await endpoint
+                .PostStringAsync(data);
+        }
+    }
+}
---
+++ Requester/MainWindowViewModel.cs
@@ -0,0 +1,73 @@
+﻿using System.Diagnostics;
+using System.Windows.Input;
+using Prism.Commands;
+using Prism.Mvvm;
+using Requester.Client;
+
+namespace Requester
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private readonly ApiClient _apiClient;
+
+        private string _endpointUrl;
+        private string _postData;
+        private bool _isBusy;
+
+        public MainWindowViewModel()
+        {
+            _apiClient = new ApiClient();
+
+            PostRequestCommand = new DelegateCommand(PostRequest, CanPostRequest)
+                .ObservesProperty(() => EndpointUrl)
+                .ObservesProperty(() => PostData)
+                .ObservesProperty(() => IsBusy);
+        }
+
+        private bool CanPostRequest() =>
+            !IsBusy
+            && !string.IsNullOrWhiteSpace(EndpointUrl)
+            && !string.IsNullOrWhiteSpace(PostData);
+
+        private async void PostRequest()
+        {
+            IsBusy = true;
+
+            try
+            {
+                var response = await _apiClient.PostString(EndpointUrl, PostData);
+                Debug.WriteLine(response.StatusCode);
+            }
+
+            catch
+            {
+                Debug.WriteLine("Error!");
+            }
+
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+        public ICommand PostRequestCommand { get; }
+
+        public bool IsBusy
+        {
+            get => _isBusy;
+            set => SetProperty(ref _isBusy, value);
+        }
+
+        public string EndpointUrl
+        {
+            get => _endpointUrl;
+            set => SetProperty(ref _endpointUrl, value);
+        }
+
+        public string PostData
+        {
+            get => _postData;
+            set => SetProperty(ref _postData, value);
+        }
+    }
+}
